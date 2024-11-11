c2388cfa87cb32dd0ae5c2aed114367fe6e0b6d8
GraysonGilbert <graysongilbert@gmail.com>
2024-11-10 22:38:05 -0500

    Trying to figure out path planning

--- Homework 2/dh_testing.py
+++ Homework 2/dh_testing.py
@@ -48,8 +48,8 @@ d_6 = 121.8
 
 # Constructing Matrices
 
-A_1 = Matrix([[cos(theta_1), -cos(alpha_1)*sin(theta_1), sin(alpha_1)*sin(theta_1), a_1*cos(theta_1)],
-             [sin(theta_1), cos(alpha_1)*cos(theta_1), -sin(alpha_1)*cos(theta_1), a_1*sin(theta_1)],
+A_1 = Matrix([[cos(theta_1 - pi), -cos(alpha_1)*sin(theta_1 - pi), sin(alpha_1)*sin(theta_1 - pi), a_1*cos(theta_1 - pi)],
+             [sin(theta_1 - pi), cos(alpha_1)*cos(theta_1 - pi), -sin(alpha_1)*cos(theta_1 - pi), a_1*sin(theta_1 - pi)],
              [0, sin(alpha_1), cos(alpha_1), d_1],
              [0, 0, 0, 1]])
 
@@ -147,11 +147,148 @@ J = Matrix([[q_1_x_dot, q_2_x_dot, q_3_x_dot, q_4_x_dot, q_5_x_dot, q_6_x_dot],
             [q_1_z_dot, q_2_z_dot, q_3_z_dot, q_4_z_dot, q_5_z_dot, q_6_z_dot],
             [Z_1, Z_2, Z_3, Z_4, Z_5, Z_6]])
 
-J = J.subs({theta_1: pi, theta_2: 0, theta_3: 0, theta_4: 0, theta_5: 0, theta_6: 0})
+
 
 #pretty_print(A_5_wrt_0)
-#pretty_print(A_6)
+#pretty_print(A_6_wrt_0)
 
 #test = A_6_wrt_0.subs({theta_1: 0, theta_2: 0, theta_3: 0, theta_4: 0, theta_5: 0, theta_6: 0})
 
-pretty_print(J)
+#pretty_print(J)
+#pretty_print(test)
+
+#inv_test = J ** -1
+
+#pretty_print(inv_test)
+
+#___________________________________________________________________________________________________________________
+
+#Initialize Robot in Home Position
+
+theta_1_vals = [-0.001]
+theta_2_vals = [0.1]
+theta_3_vals = [0.1]
+theta_4_vals = [0.1]
+theta_5_vals = [0.001]
+theta_6_vals = [0]
+
+x_end_pos = []
+y_end_pos = []
+z_end_pos = []
+
+# Initialize x,yz, velocities
+x_dot = [0]
+y_dot = [0]
+z_dot = []
+alpha_dot = [0]
+beta_dot = [0]
+gamma_dot = [0]
+
+# Setting Up Time
+z_val = []
+total_steps = 10
+step = 30/total_steps
+
+time = np.linspace(0, 30, total_steps)
+
+for t in np.arange(0, 30, step):
+    z_dot.append(-1)
+
+#print(z_dot)
+
+# Drawing Specifications:
+
+# Circle Specifications
+
+r = 5
+a = 0
+b = 0
+
+# Moving to start point
+
+
+
+
+# Main Loop
+
+for i in range(0, total_steps):
+
+    x_vals = Matrix([x_dot[0], y_dot[0], z_dot[i], alpha_dot[0], beta_dot[0], gamma_dot[0]])
+
+    J_sub = J.subs({theta_1: theta_1_vals[i], theta_2: theta_2_vals[i], theta_3: theta_3_vals[i], theta_4: theta_4_vals[i], theta_5: theta_5_vals[i], theta_6: theta_6_vals[i]})
+
+    inv_J = J_sub ** -1
+
+    q = inv_J * x_vals
+
+    #q = q_dot * step
+
+    theta_1_vals.append(theta_1_vals[i-1] + (float(q[0]) * step))
+    theta_2_vals.append(theta_2_vals[i-1] + (float(q[1]) * step))
+    theta_3_vals.append(theta_3_vals[i-1] + (float(q[2]) * step))
+    theta_4_vals.append(theta_4_vals[i-1] + (float(q[3]) * step))
+    theta_5_vals.append(theta_5_vals[i-1] + (float(q[4]) * step))
+    theta_6_vals.append(theta_6_vals[i-1] + (float(q[5]) * step))
+
+
+# Collecting Calculated Joint Angles
+
+
+
+
+
+print("theta values 2,3,4")
+#print(theta_1_vals)
+print(theta_2_vals)
+print(theta_3_vals)
+print(theta_4_vals)
+#print(theta_5_vals)
+#print(theta_6_vals)
+
+# Plugging Calculated Joint Angles Back into Forward Kinematic Equation
+
+
+
+# Verifying Solution by Plugging in calculated theta values
+for i in range(0, total_steps):
+
+    end_pos = A_6_wrt_0.subs({theta_1: theta_1_vals[i], theta_2: theta_2_vals[i], theta_3: theta_3_vals[i], theta_4: theta_4_vals[i], theta_5: theta_5_vals[i], theta_6: theta_6_vals[i]})
+
+    x_pos = end_pos[0, 3]
+    y_pos = end_pos[1, 3]
+    z_pos = end_pos[2, 3]
+
+
+    x_end_pos.append(x_pos)
+    y_end_pos.append(y_pos)
+    z_end_pos.append(z_pos)
+
+
+print(x_end_pos)
+print(y_end_pos)
+print(z_end_pos)
+
+# Plotting Relevant Information
+
+
+# Plotting Relevant Information
+
+# Plot of Theta Values Vs. Time
+plt.figure(1)
+plt.title('Theta Values Over Time')
+plt.plot(time, theta_1_vals[1:], label='theta 1')
+plt.plot(time, theta_2_vals[1:], label='theta 2')
+plt.plot(time, theta_3_vals[1:], label='theta 3')
+plt.plot(time, theta_4_vals[1:], label='theta 4')
+plt.plot(time, theta_5_vals[1:], label='theta 5')
+plt.plot(time, theta_6_vals[1:], label='theta 6')
+
+plt.legend()
+
+# Plot of End Effector Trajectory
+plt.figure(2)
+plt.title('End Effector Trajectory')
+plt.plot(z_end_pos, x_end_pos)
+
+
+plt.show()
