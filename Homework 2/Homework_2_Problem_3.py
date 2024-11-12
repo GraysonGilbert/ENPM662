418043d7819a73bd98b1c642cbddc45465cbc25d
GraysonGilbert <graysongilbert@gmail.com>
2024-11-12 17:16:54 -0500

    trying to debug inverse kinematics

--- Homework 2/Homework_2_Problem_3.py
+++ Homework 2/Homework_2_Problem_3.py
@@ -13,8 +13,8 @@ theta_1, theta_2, theta_3, theta_4, theta_5, theta_6 = symbols('theta_1 theta_2 
 
 
 a_1 = 0
-a_2 = -.73731
-a_3 = -.3878
+a_2 = -737.31
+a_3 = -387.8
 a_4 = 0
 a_5 = 0
 a_6 = 0
@@ -26,12 +26,12 @@ alpha_4 = pi/2
 alpha_5 = -pi/2
 alpha_6 = 0
 
-d_1 = .1833
+d_1 = 183.3
 d_2 = 0
 d_3 = 0
-d_4 = 0.0955
-d_5 = .1155
-d_6 = .1218
+d_4 = 95.5
+d_5 = 115.5
+d_6 = 121.8
 
 
 
@@ -80,6 +80,8 @@ A_4_wrt_0 = A_3_wrt_0 * A_4
 A_5_wrt_0 = A_4_wrt_0 * A_5
 
 A_6_wrt_0 = simplify(A_5_wrt_0 * A_6)
+
+A_2_wrt_4 = simplify(A_2*A_3*A_4)
 
 
 # Collecting Z Values
@@ -138,153 +140,3 @@ J = Matrix([[q_1_x_dot, q_2_x_dot, q_3_x_dot, q_4_x_dot, q_5_x_dot, q_6_x_dot],
 
 
 #___________________________________________________________________________________________________________________
-
-#Initialize Robot in Home Position
-theta_1_vals = [0]
-theta_2_vals = [0.01]
-theta_3_vals = [-.0383]
-theta_4_vals = [0.0283]
-theta_5_vals = [0.0001]
-theta_6_vals = [0]
-
-x_end_pos = []
-y_end_pos = []
-z_end_pos = []
-
-# Initialize x,yz, velocities
-x_dot = [0]
-y_dot = [0]
-z_dot = []
-alpha_dot = [0]
-beta_dot = [0]
-gamma_dot = [0]
-
-# Setting Up Time 
-z_val = []   
-home_2_start_steps = 5
-step = 3/home_2_start_steps
-
-time = np.linspace(0, 3, home_2_start_steps)
-
-home_2_start_z_dot = -.49778 / home_2_start_steps
-print(home_2_start_z_dot)
-
-#for t in np.arange(0, 30, step):
-#    z_dot.append(z_dot_val)
-
-#print(z_dot)
-
-# Drawing Specifications:
-
-# Circle Specifications
-
-r = 5
-a = 0
-b = 0
-
-# Moving to start point
-
-
-J_sub_test = J.subs({theta_1: theta_1_vals[0], theta_2: theta_2_vals[0], theta_3: theta_3_vals[0], theta_4: theta_4_vals[0], theta_5: theta_5_vals[0], theta_6: theta_6_vals[0]})
-
-pretty_print(J_sub_test)
-
-# Main Loop
-for i in range(0, home_2_start_steps):
-
-    x_vals = Matrix([x_dot[0], y_dot[0], home_2_start_z_dot, alpha_dot[0], beta_dot[0], gamma_dot[0]])
-
-    #print("x_vals:")
-    #print(x_vals)
-
-    J_sub = J.subs({theta_1: theta_1_vals[i], theta_2: theta_2_vals[i], theta_3: theta_3_vals[i], theta_4: theta_4_vals[i], theta_5: theta_5_vals[i], theta_6: theta_6_vals[i]})
-
-    #print("Jacobian:" + str(i))
-    #pretty_print(J_sub)
-
-    inv_J = J_sub ** -1
-
-    #print("Inverse Jacobian:" + str(i))
-    #pretty_print(inv_J)
-
-    q = inv_J * x_vals
-
-    print("q(really its q_dot):" + str(i))
-
-    pretty_print(q)
-
-    #q = q_dot * step
-
-    # Collecting Calculated Joint Angles
-
-    theta_1_vals.append(theta_1_vals[i-1] + (float(q[0]) * step))
-    theta_2_vals.append(theta_2_vals[i-1] + (float(q[1]) * step))
-    theta_3_vals.append(theta_3_vals[i-1] + (float(q[2]) * step))
-    theta_4_vals.append(theta_4_vals[i-1] + (float(q[3]) * step))
-    theta_5_vals.append(theta_5_vals[i-1] + (float(q[4]) * step))
-    theta_6_vals.append(theta_6_vals[i-1] + (float(q[5]) * step))
-
-    #print("theta_2_vals")
-    #print(theta_2_vals)
-
-
-
-
-
-#print("theta values 2,3,4")
-#print(theta_1_vals)
-#print(theta_2_vals)
-#print(theta_3_vals)
-#print(theta_4_vals)
-#print(theta_5_vals)
-#print(theta_6_vals)
-
-
-
-
-
-# Verifying Solution by Plugging in calculated theta values
-
-for i in range(0, home_2_start_steps):
-
-    # Plugging Calculated Joint Angles Back into Forward Kinematic Equation
-
-    end_pos = A_6_wrt_0.subs({theta_1: theta_1_vals[i], theta_2: theta_2_vals[i], theta_3: theta_3_vals[i], theta_4: theta_4_vals[i], theta_5: theta_5_vals[i], theta_6: theta_6_vals[i]})
-
-    x_pos = end_pos[0, 3]
-    y_pos = end_pos[1, 3]
-    z_pos = end_pos[2, 3]
-
-
-    x_end_pos.append(x_pos)
-    y_end_pos.append(y_pos)
-    z_end_pos.append(z_pos)
-
-
-#print(x_end_pos)
-#print(y_end_pos)
-#print(z_end_pos)
-
-# Plotting Relevant Information
-
-
-# Plotting Relevant Information
-
-# Plot of Theta Values Vs. Time
-plt.figure(1)
-plt.title('Theta Values Over Time')
-plt.plot(time, theta_1_vals[1:], label='theta 1')
-plt.plot(time, theta_2_vals[1:], label='theta 2')
-plt.plot(time, theta_3_vals[1:], label='theta 3')
-plt.plot(time, theta_4_vals[1:], label='theta 4')
-plt.plot(time, theta_5_vals[1:], label='theta 5')
-plt.plot(time, theta_6_vals[1:], label='theta 6')
-
-plt.legend()
-
-# Plot of End Effector Trajectory
-plt.figure(2)
-plt.title('End Effector Trajectory')
-plt.plot(x_end_pos, z_end_pos)
-
-plt.show()
